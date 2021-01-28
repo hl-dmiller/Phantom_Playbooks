e2cc56797a85fa24086cfedb126d4b4ac4320219
Dustin Miller <dmiller@hurricanelabs.com>
2021-01-28 21:16:47 +0000

    Lab 6 (2)

--- Log File Hashes.py
+++ Log File Hashes.py
@@ -6,6 +6,82 @@ import json
 from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
+    
+    # call 'filter_1' block
+    filter_1(container=container)
+
+    return
+
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('filter_1() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "!=", ""],
+        ],
+        name="filter_1:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        decision_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('decision_1() called')
+
+    # check for 'if' condition 1
+    matched = phantom.decision(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "in", "custom_list:Prior Hashes"],
+        ])
+
+    # call connected blocks if condition 1 matched
+    if matched:
+        format_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+        return
+
+    # call connected blocks for 'else' condition 2
+    add_list_3(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+
+    return
+
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('format_1() called')
+    
+    template = """File with hash {0} has been seen before."""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "artifact:*.cef.fileHash",
+    ]
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    add_comment_2(container=container)
+
+    return
+
+def add_comment_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('add_comment_2() called')
+
+    formatted_data_1 = phantom.get_format_data(name='format_1')
+
+    phantom.comment(container=container, comment=formatted_data_1)
+
+    return
+
+def add_list_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('add_list_3() called')
+
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.fileHash', 'artifact:*.id'])
+
+    container_item_0 = [item[0] for item in container_data]
+
+    phantom.add_list("Prior Hashes", container_item_0)
 
     return
 
